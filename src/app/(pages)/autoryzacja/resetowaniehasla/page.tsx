d7f6fb6ea98269879eff4a687d901dfd8504f728
ZurcieK <jazdanrajabi@gmail.com>
2025-02-18 17:41:49 +0100

    dsadasasda

--- src/app/(pages)/autoryzacja/resetowaniehasla/page.tsx
+++ src/app/(pages)/autoryzacja/resetowaniehasla/page.tsx
@@ -4,7 +4,7 @@ import { ResetCard } from "@/app/components/authComponents/ResetCard";
 
 const Page = () => {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
       <div className="flex justify-center items-center h-screen  ">
         <div className="">
           <ResetCard />
