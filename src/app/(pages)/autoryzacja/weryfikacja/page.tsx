ec8c2a29de92c3499ba17f088c802e4316d4562c
ZurcieK <jazdanrajabi@gmail.com>
2025-02-19 19:26:48 +0100

    verify

--- src/app/(pages)/autoryzacja/weryfikacja/page.tsx
+++ src/app/(pages)/autoryzacja/weryfikacja/page.tsx
@@ -1,42 +1,50 @@
 /* eslint-disable */
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 import axiosInstance from "../../../../../api/axiosInstance";
-import { Suspense } from 'react';
+import { Suspense } from "react";
 
-const ConfirmPage: React.FC = () => {
-  const [status, setStatus] = useState<string | null>(null); 
+function Verify() {
+  const [status, setStatus] = useState<string | null>(null);
   const searchParams = useSearchParams();
-  const token = searchParams.get('token');
+  const token = searchParams.get("token");
 
   useEffect(() => {
     if (token) {
       axiosInstance
-        .get(`/auth/verify`, { params: { token} })   
+        .get(`/auth/verify`, { params: { token } })
         .then((response) => {
-          setStatus("Konto zostało pomyślnie potwierdzone. Możesz powrócić do strony logowania.");
-        })  
+          setStatus(
+            "Konto zostało pomyślnie potwierdzone. Możesz powrócić do strony logowania."
+          );
+        })
         .catch((error) => {
-          setStatus("Błąd weryfikacji konta. Użytkownik może być już potwierdzony.");
+          setStatus(
+            "Błąd weryfikacji konta. Użytkownik może być już potwierdzony."
+          );
         });
     }
   }, [token]);
 
   return (
-    <Suspense fallback={<div>Ładowanie...</div>}>
-      <div className="h-screen flex items-center justify-center bg-white">
-        <div className="max-w-lg text-center p-6">
-          {status ? (
-            <div className="text-lg text-green-600">{status}</div>
-          ) : (
-            <div className="text-lg text-gray-600">Trwa weryfikacja...</div>
-          )}
-        </div>
+    <div className="h-screen flex items-center justify-center bg-white">
+      <div className="max-w-lg text-center p-6">
+        {status ? (
+          <div className="text-lg text-green-600">{status}</div>
+        ) : (
+          <div className="text-lg text-gray-600">Trwa weryfikacja...</div>
+        )}
       </div>
-    </Suspense>
+    </div>
   );
+}
+
+const ConfirmPage: React.FC = () => {
+  return <Suspense fallback={<div>Ładowanie...</div>}>
+    <Verify/>
+  </Suspense>;
 };
 
 export default ConfirmPage;
